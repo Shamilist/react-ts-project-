9f95b9c339c7e282fb3d063bb277492cbba72fcf
Shamil Kh <shamilist90@gmail.com>
2023-08-16 21:53:23 +0300

    add Product

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React, {useState} from 'react';
+import {Product} from './components/Product'
+import {products} from './data/products'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="container">
-    <h1 className='font-bold' key={1}>React user</h1>
-    <button key={2}>Button</button>
-    </div>
-
+<div className="container mx-auto max-w-2xl pt-5">
+  <Product product={products[0]}/>
+</div>
   );
 }
 
