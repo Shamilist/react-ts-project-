18aa67fae08f35f94b33a3add37fb4d48b86ffbb
Shamil Kh <shamilist90@gmail.com>
2023-08-17 18:09:22 +0300

    add button log

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,16 +7,21 @@ interface ProductProps {
 
 export function Product({ product }: ProductProps) {
 
-    const [details, setDetails] = useState(false)
+    const [details, setDetails] = useState(false);
+
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+    const buttonClasses = ['border py-2 px-4 ', btnBgClassName];
 
     return(
         <div
         className='border py-2 px-4 rounded flex flex-col items-center mb-2'>
             <img src={ product.image } className='w-1/6' alt={ product.title} />
             <p>{ product.title }</p>
-            <p className='font-bold'>{product.price}</p>
-            <button className='py-2 px-4 border bg-green-400'
-            onClick={() => setDetails(prev => !prev) }>Details</button>
+            <p className='font-bold'>{product.price}$</p>
+            <button className={buttonClasses.join(' ')}
+            onClick={() => setDetails(prev => !prev) }>
+                { details ? "Hide details" : 'Show details'}
+                </button>
 
 
             {details && <div >
