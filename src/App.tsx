a38375e30aa38e514fd686022e9df304de78469b
Shamil Kh <shamilist90@gmail.com>
2023-08-18 14:37:18 +0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
-import React, { useContext} from 'react';
-import {Product} from './components/Product'
-import { useProducts } from './hooks/products';
-import { Loader } from './components/Loader';
-import { ErrorMessage } from './components/ErrorMessage'
-import { Modal } from './components/Modal'
-import { CreateProduct } from './components/CreateProduct'
-import { IProduct } from './models';
-import { ModalContext } from './context/ModalContext';
+import { Route, Routes } from "react-router-dom";
+import { ProductPage } from './pages/ProductsPage'
+import { AboutPage } from './pages/AboutPage'
 
 
 
 function App() {
+return (
+<Routes>
+  <Route path="/" element={ <ProductPage/> }/>
+  <Route path="/about" element={ <AboutPage/> }/>
+</Routes>
+)
 
-const {loading, error, products, addProduct} = useProducts();
-const { modal, open, close} = useContext(ModalContext);
-
-const createHandler = (product: IProduct) => {
-  close(); 
-  addProduct(product);
-}
-
-  return (
-  <div className="container mx-auto max-w-2xl pt-5">
-  
-  { loading && <Loader />}
-  { error && <ErrorMessage error={ error } /> }
-  { products.map((product) => <Product product={product} key={product.id}/>)}
-
-  { modal && <Modal title='Create new product' onClose={() => close}>
-    <CreateProduct onCreate={createHandler}/>
-  </Modal>}
-
-<button className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
-onClick={open}>Add item</button>
-</div>
-  );
 }
 
 export default App;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio dolor numquam delectus labore deleniti quos obcaecati sapiente libero aliquid quaerat reprehenderit architecto, ad incidunt. Accusantium reprehenderit unde quod debitis molestias.</div>
+    )
+}
