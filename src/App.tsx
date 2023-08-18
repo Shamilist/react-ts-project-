3c2b818abe96dc95243ed6209f235e27eb8ad3ea
Shamil Kh <shamilist90@gmail.com>
2023-08-18 10:35:09 +0300

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import {Product} from './components/Product'
 import { useProducts } from './hooks/products';
 import { Loader } from './components/Loader';
 import { ErrorMessage } from './components/ErrorMessage'
+import { Modal } from './components/Modal'
+import { CreateProduct } from './components/CreateProduct'
 
 
 
@@ -15,6 +17,10 @@ const {loading, error, products} = useProducts();
   { loading && <Loader />}
   { error && <ErrorMessage error={ error } /> }
   { products.map((product) => <Product product={product} key={product.id}/>)}
+
+  <Modal>
+    <CreateProduct/>
+  </Modal>
 
 </div>
   );
